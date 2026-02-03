c33ebbdd8aa1dc552a1ec87583920fc711ae2874
Brayden Peeples <braydenpeeples@gmail.com>
2026-02-03 12:06:57 -0500

    Completed M3Login_Peeples

--- M3Login_Peeples/ViewController.swift
+++ M3Login_Peeples/ViewController.swift
@@ -9,6 +9,36 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var idTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    @IBAction func loginButton(_ sender: Any) {
+        let id = idTextField.text!
+        let pw = passwordTextField.text!
+        // id: bpeeples
+        // pw: Pa$$w0rd
+        
+        //if id == "bpeeples" && pw == "Pa$$w0rd" {
+        //    resultLabel.text = "Login Success!"
+        //    print("login success")
+        //}
+        //else {
+        //    resultLabel.text = "Login Failed!"
+        //    print("login failed")
+        //if id.isEmpty || pw.isEmpty {
+        //    resultLabel.text = "Please input ID and Password!"
+        //    return // early exit
+        //}
+        // guard condition else{return}
+        guard id.isEmpty || pw.isEmpty else {
+            resultLabel.text = "Please input ID and Password!"
+            return
+        }
+        resultLabel.text = (id == "bpeeples" && pw == "Pa$$w0rd") ? "Login Success!" : "Login Failed!"
+    
+    }
+    @IBOutlet weak var resultLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
