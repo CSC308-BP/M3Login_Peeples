ce21afbdbd1612272457705d441e759cab21c88e
Brayden Peeples <braydenpeeples@gmail.com>
2026-02-26 11:54:35 -0500

    Updated with guards.

--- M3Login_Peeples/ViewController.swift
+++ M3Login_Peeples/ViewController.swift
@@ -9,12 +9,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    // Logical NOT: !expr
+    // Forced Unwrapping: optional_expr!
+    // Implicit Unwrapped Optionals (IUO): type!
     @IBOutlet weak var idTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var resultLabel: UILabel!
     
     @IBAction func loginButton(_ sender: Any) {
-        let id = idTextField.text!
-        let pw = passwordTextField.text!
+        guard let id = idTextField.text, !id.isEmpty else {
+            showAlert(message: "ID must not be empty!")
+            return
+        }
+        guard let pw = passwordTextField.text, !pw.isEmpty else {
+            showAlert(message: "Password must not be empty!")
+            return
+        }
+        
         // id: bpeeples
         // pw: Pa$$w0rd
         
@@ -30,20 +42,30 @@ class ViewController: UIViewController {
         //    return // early exit
         //}
         // guard condition else{return}
-        guard id.isEmpty || pw.isEmpty else {
-            resultLabel.text = "Please input ID and Password!"
-            return
-        }
+        //guard id.isEmpty || pw.isEmpty else {
+        //    resultLabel.text = "Please input ID and Password!"
+        //    return
+        //}
         resultLabel.text = (id == "bpeeples" && pw == "Pa$$w0rd") ? "Login Success!" : "Login Failed!"
     
     }
-    @IBOutlet weak var resultLabel: UILabel!
+    
+    
+    func showAlert(message: String) {
+        let alert = UIAlertController(title: "Alert", message: message, preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "OK", style: .default)
+        alert.addAction(okAction)
+        present(alert, animated: true, completion: nil)
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
 
+    override func viewDidAppear(_ animated: Bool) {
+        idTextField.becomeFirstResponder()
+    }
 
 }
 
